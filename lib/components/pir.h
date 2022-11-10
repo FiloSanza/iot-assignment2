5c92e7ea700bb96993f959e48238951beb548332
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2022-11-10 12:28:47 +0100

    Add pir implementation.

--- lib/components/pir.h
+++ lib/components/pir.h
@@ -1,6 +1,19 @@
 #ifndef __PIR_H_INCLUDED
 #define __PIR_H_INCLUDED
 
+#include "lib.h"
 
+namespace Components {
+  class Pir {
+    public:
+      Pir();
+      Pir(uint8_t pin);
+
+      bool read();
+
+  private:
+    uint8_t pin;
+  };
+}
 
 #endif
