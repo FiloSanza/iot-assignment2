bc0824b88365df12333ed5196503227a8d27e5a6
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2022-11-10 11:43:21 +0100

    Add lib utility file

---
+++ include/lib.h
@@ -0,0 +1,7 @@
+#ifndef __LIB_H_INCLUDED
+#define __LIB_H_INCLUDED
+
+#include <cstdint>
+#include <Arduino.h>
+
+#endif
--- lib/components/potentiometer.h
+++ lib/components/potentiometer.h
@@ -1,6 +1,25 @@
 #ifndef __POTENTIOMETER_H_INCLUDED
 #define __POTENTIOMETER_H_INCLUDED
 
+#include "lib.h"
 
+namespace Components {
+    class Potentiometer {
+    public:
+        Potentiometer(uint8_t pin, uint16_t lower_bound, uint16_t upper_bound);
+
+        void setUpperBound(uint16_t value);
+        void setLowerBound(uint16_t value);
+
+        uint16_t getUpperBound() const;
+        uint16_t getLowerBound() const;
+        uint16_t read() const;
+
+    private:
+        uint8_t pin;
+        uint16_t lower_bound;
+        uint16_t upper_bound;
+    };
+}
 
 #endif
